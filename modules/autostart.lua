d38858c80eeb02196a4906ecf8fed5a5b80523f1
haencyl <git@haencyl.dev>
2026-07-06 02:10:00 +0200

    feat: default to noctalia polkit agent

--- modules/autostart.lua
+++ modules/autostart.lua
@@ -4,7 +4,6 @@
 
 hl.on("hyprland.start", function()
   hl.exec_cmd("noctalia")
-  hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
   hl.exec_cmd("hyprpm reload -n")
   hl.exec_cmd("systemctl --user import-environment $(env | cut -d'=' -f 1)")
   hl.exec_cmd("dbus-update-activation-environment --systemd --all")
